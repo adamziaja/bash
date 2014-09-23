092a254f502cdb9e337b20749573e7c2a3674970
Adam Ziaja <adamziaja@users.noreply.github.com>
2014-09-23 18:44:36 +0200

    Update axfr.sh

--- axfr.sh
+++ axfr.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-domain=$1
-for dns in $(host -t ns $domain | awk '{print $4}');do dig @$dns axfr $domain;done
+domain=$1;for dns in $(host -t ns $domain | awk '{print $4}');do dig @$dns axfr $domain;done
