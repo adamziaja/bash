a96847c9fa74cf8d8485ef3e4e121f82c41e3440
Adam Ziaja <adamziaja@users.noreply.github.com>
2015-02-06 10:38:37 +0100

    Update connectivity.sh

--- connectivity.sh
+++ connectivity.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 echo "########## START ($1): `date` ##########"
+# ICMP
 echo "ping -c 1 $1"
 ping -c 1 $1
 echo '####################'
+# ICMP
 echo "mtr --no-dns $1 --report --report-cycles=1"
 mtr --no-dns $1 --report --report-cycles=1
 echo '####################'
+# ICMP
 traceroute $1
 echo '####################'
+# TCP
 printf 'tcp'
 tcptraceroute $1
 echo '####################'
+# TCP
 echo "nmap -F -Pn $1 --open"
 nmap -F -Pn $1 --open
 echo '####################'
+# UDP
 echo "nmap -F -Pn -sU $1 --open"
 nmap -F -Pn -sU $1 --open
 echo "########## END ($1): `date` ##########"
