b0e52862e97e40f2aa4a56bbce8ae407e8b994c0
Adam Ziaja <adamziaja@users.noreply.github.com>
2015-02-09 11:05:00 +0100

    Update connectivity.sh

--- connectivity.sh
+++ connectivity.sh
@@ -5,28 +5,30 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
+S="########################################"
+
 for IP in "$@"
 do
-	echo -e "\e[45mSTART ($IP): `date`\e[0m"
+        echo -e "\e[45mSTART ($IP): `date`\e[0m"
 
-	echo -e "\e[44mICMP:\e[42mping -c 1 $IP\e[0m"
-	ping -c 1 $IP
+        echo -e "$S\n\e[44mICMP:\e[42mping -c 1 $IP\e[0m"
+        ping -c 1 $IP
 
-	echo -e "\e[44mICMP:\e[42mmtr --no-dns $IP --report --report-cycles=1\e[0m"
-	mtr --no-dns $IP --report --report-cycles=1
+        echo -e "$S\n\e[44mICMP:\e[42mmtr --no-dns $IP --report --report-cycles=1\e[0m"
+        mtr --no-dns $IP --report --report-cycles=1
 
-	echo -e "\e[44mICMP:\e[42mtraceroute $IP\e[0m"
-	traceroute $IP
+        echo -e "$S\n\e[44mICMP:\e[42mtraceroute $IP\e[0m"
+        traceroute $IP
 
-	echo -e "\e[44mTCP:\e[42mtcptraceroute $IP\e[0m"
-	tcptraceroute $IP
+        echo -e "$S\n\e[44mTCP:\e[42mtcptraceroute $IP\e[0m"
+        tcptraceroute $IP
 
-	echo -e "\e[44mTCP:\e[42mnmap -F -Pn $IP --open\e[0m"
-	nmap -F -Pn $IP --open
+        echo -e "$S\n\e[44mTCP:\e[42mnmap -F -Pn $IP --open\e[0m"
+        nmap -F -Pn $IP --open
 
-	echo -e "\e[44mUDP:\e[42mnmap -F -Pn -sU $IP --open\e[0m"
-	nmap -F -Pn -sU $IP --open | grep -v 'Host is up'
-	# -Pn: Treat all hosts as online -- skip host discovery
+        echo -e "$S\n\e[44mUDP:\e[42mnmap -F -Pn -sU $IP --open\e[0m"
+        nmap -F -Pn -sU $IP --open | grep -v 'Host is up'
+        # -Pn: Treat all hosts as online -- skip host discovery
 
-	echo -e "\e[45mEND ($IP): `date`\e[0m"
+        echo -e "$S\n\e[45mEND ($IP): `date`\e[0m"
 done
