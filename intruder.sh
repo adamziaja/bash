84ecde470f0707ae650ae42af33b48d0e9158e64
Adam Ziaja <adamziaja@users.noreply.github.com>
2015-03-23 23:40:52 +0100

    Update intruder.sh

--- intruder.sh
+++ intruder.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # for https://github.com/adamziaja/python/blob/master/ske.py
-for expl in $(ls -1)
-do
+for expl in $(ls -1);do
   echo $expl
   chmod +x $expl
   if [ "`echo $expl | awk -F'.' '{print $2}'`" == "sh" ];then
